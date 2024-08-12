f7d316f9bd7f4a1dbdbc4a68c15d8d80527a0d9e
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-08-13 00:16:02 +0200

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,32 @@
+
+#include <vector>
+using namespace std;
+
+class Solution {
+
+public:
+    int projectionArea(const vector<vector<int>>& squareMatrix) const {
+        int sideLength = squareMatrix.size();
+        int totalProjectedArea = 0;
+
+        for (int row = 0; row < sideLength; ++row) {
+            int maxColumnHeight = 0;
+            int maxRowHeight = 0;
+
+            for (int column = 0; column < sideLength; ++column) {
+                if (squareMatrix[row][column] > 0) {
+                    ++totalProjectedArea;
+                }
+                if (maxRowHeight < squareMatrix[row][column]) {
+                    maxRowHeight = squareMatrix[row][column];
+                }
+                if (maxColumnHeight < squareMatrix[column][row]) {
+                    maxColumnHeight = squareMatrix[column][row];
+                }
+            }
+            totalProjectedArea += maxRowHeight + maxColumnHeight;
+        }
+
+        return totalProjectedArea;
+    }
+};
